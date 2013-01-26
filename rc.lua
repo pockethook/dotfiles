96e87b2b50b5ffa7e5b8ade0961470abd97d934a
Cheehan <pockethook@gmail.com>
2013-01-26 13:27:03 +0000

    Awesome browser shortcut conflict.

--- rc.lua
+++ rc.lua
@@ -245,7 +245,7 @@ globalkeys = awful.util.table.join(
 
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
-    awful.key({ modkey,           }, "w", function () awful.util.spawn(browser) end),
+    awful.key({ modkey,           }, "a", function () awful.util.spawn(browser) end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
