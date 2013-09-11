df90816c7a35015a92ddeea7f129ecaddd4249fb
Cheehan Weereratne <pockethook@gmail.com>
2013-09-11 17:43:12 +0100

    Directory link fix.

--- link.sh
+++ link.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-ln -sf $PWD/.zsh ~/.zsh
 ln -sf $PWD/zshrc ~/.zshrc
 ln -sf $PWD/bash_profile ~/.bash_profile
 ln -sf $PWD/bashrc ~/.bashrc
@@ -10,5 +9,6 @@ ln -sf $PWD/vimrc ~/.vimrc
 ln -sf $PWD/xinitrc ~/.xinitrc
 ln -sf $PWD/Xresources ~/.Xresources
 ln -sf $PWD/Xmodmap ~/.Xmodmap
+ln -sf $PWD/zsh ~/.zsh
 ln -sf $PWD/zshenv ~/.zshenv
 ln -sf $PWD/zshrc ~/.zshrc
