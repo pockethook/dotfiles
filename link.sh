127aec136b0fa2f8a34fc9ce8480213ea6388317
Cheehan <pockethook@gmail.com>
2013-10-13 18:02:18 +0100

    Typo.

--- link.sh
+++ link.sh
@@ -3,5 +3,5 @@ ln -sf $PWD/Xresources ~/.Xresources
 ln -sf $PWD/bashrc ~/.bashrc
 ln -sf $PWD/config ~/.i3/config
 ln -sf $PWD/i3status.conf ~/.i3status.conf
-ln -sf $PWD/vimrc ~/.vimirc
+ln -sf $PWD/vimrc ~/.vimrc
 ln -sf $PWD/xinitrc ~/.xinitrc
