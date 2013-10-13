74f6103963d0335d0472ff91138602760d50104a
Cheehan <pockethook@gmail.com>
2013-10-13 16:14:12 +0100

    Use current directory.

--- link.sh
+++ link.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-mkdir -p /home/pockethook/backup
-mv /home/pockethook/.{bashrc,vimrc,xinitrc,Xresources} /home/pockethook/backup
-mv /home/pockethook/.config/awesome/rc.lua /home/pockethook/backup
-mv /home/pockethook/.config/awesome/themes/custom/theme.lua /home/pockethook/backup
-mkdir -p /home/pockethook/.config/awesome/themes/custom
-ln -s /home/pockethook/dotfiles/bashrc /home/pockethook/.bashrc
-ln -s /home/pockethook/dotfiles/rc.lua /home/pockethook/.config/awesome/rc.lua
-ln -s /home/pockethook/dotfiles/theme.lua /home/pockethook/.config/awesome/themes/custom/theme.lua
-ln -s /home/pockethook/dotfiles/vimrc /home/pockethook/.vimrc
-ln -s /home/pockethook/dotfiles/xinitrc /home/pockethook/.xinitrc
-ln -s /home/pockethook/dotfiles/Xresources /home/pockethook/.Xresources
+ln -sf $PWD/Xresources ~/.Xresources
+ln -sf $PWD/bashrc ~/.bashrc
+ln -sf $PWD/config ~/.i3/config
+ln -sf $PWD/i3status.conf ~/.i3status.conf
+ln -sf $PWD/vimrc ~/.vimirc
+ln -sf $PWD/xinitrc ~/.xinitrc
