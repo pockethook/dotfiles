5c3fce486fa58a962ac3f71cb3c23f95fb0382d9
Cheehan Weereratne <pockethook@gmail.com>
2013-11-11 20:47:48 +0000

    Remove bash.

--- link.sh
+++ link.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-ln -sf $PWD/bash_profile ~/.bash_profile
-ln -sf $PWD/bashrc ~/.bashrc
 ln -sf $PWD/config ~/.i3/config
 ln -sf $PWD/dircolors ~/.dircolors
 ln -sf $PWD/i3status.conf ~/.i3status.conf
