7942f78d241fc132dc04a2ddd6f1f9d1998fcf19
Cheehan <pockethook@npyu.eu>
2014-05-17 16:49:53 +0100

    Backup old dotfiles.

--- link.sh
+++ link.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-ln -sf $PWD/config ~/.i3/config
-ln -sf $PWD/dircolors ~/.dircolors
-ln -sf $PWD/i3status.conf ~/.i3status.conf
-ln -sf $PWD/vimrc ~/.vimrc
-ln -sf $PWD/xinitrc ~/.xinitrc
-ln -sf $PWD/Xresources ~/.Xresources
-ln -sf $PWD/zprofile ~/.zprofile
-ln -sf $PWD/zsh ~/.zsh
-ln -sf $PWD/zshenv ~/.zshenv
-ln -sf $PWD/zshrc ~/.zshrc
+
+dotfiles=$PWD
+
+mkdir backup
+
+for f in $(ls | grep -v 'README.md\|link.sh\|backup')
+do
+	mv ~/.$f $PWD/backup
+	ln -svf $dotfiles/$f ~/.$f
+done
