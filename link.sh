69b62b20fa26bef313cd5d910f4aab103e87def1
Cheehan Weereratne <pockethook@gmail.com>
2014-05-17 15:33:53 +0100

    Remove unused files.

--- link.sh
+++ link.sh
@@ -5,7 +5,6 @@ ln -sf $PWD/i3status.conf ~/.i3status.conf
 ln -sf $PWD/vimrc ~/.vimrc
 ln -sf $PWD/xinitrc ~/.xinitrc
 ln -sf $PWD/Xresources ~/.Xresources
-ln -sf $PWD/Xmodmap ~/.Xmodmap
 ln -sf $PWD/zprofile ~/.zprofile
 ln -sf $PWD/zsh ~/.zsh
 ln -sf $PWD/zshenv ~/.zshenv
